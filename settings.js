b12f04170d0b611628f83667cce79373da207214
InduwarOfficial <99625094+InduwarOfficial@users.noreply.github.com>
2022-07-18 12:09:55 +0530

    Create settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is 𝙄𝙣𝙙𝙪𝙬𝙖𝙧𝙖 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡
-My Creator is Induwara Thathsara Kumarathunga` //Costomize Alive Message (type your message in `` )
+My Creator is Induwara Thathsara Kumarathunga😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+94764247716'] //Owner number in aive msg
 global.premium = ['+94764247716'] //Owner Number info
